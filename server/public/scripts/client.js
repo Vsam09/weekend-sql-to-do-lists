acb063c551e40cf855f139e74cb9601763b02cb9
samson vang <vangsam2@gmail.com>
2021-08-06 23:35:19 -0500

    stuck with getting Complete button to highlight entire row to green

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,8 +3,15 @@ $(document).ready(onReady);
 function onReady() {
     $('#postBtn').on('click', postTasks)
     $(document).on('click','.deleteBtn', deleteTask )
+    $(document).on('click','.completeBtn', generateYellow)
     getTaskData();
 };
+
+//Create button to change background color to yellow
+function generateYellow() {
+    $(this).closest('td').addClass('yellowBackground')
+    console.log('WORK')
+}//End yellow button
 
 function deleteTask(){
     // console.log('Delete Button working..', $(this))
@@ -29,12 +36,12 @@ function getTaskData() {
         // append data to the DOM
         for (let i = 0; i < response.length; i++) {
             $('#taskTableBody').append(`
-                <tr data-id="${response[i].id}">
+                <tr id="yellowBackground" data-id="${response[i].id}">
                     <td>${response[i].task}</td>
                     <td>${response[i].date}</td>
 
-                    <td><button class="completeBtn">Complete</button></td>
-                    <td><button class="deleteBtn">Delete</button></td>
+                    <td><button class="yellowBackground class="completeBtn">Complete</button></td>
+                    <td><button id="redBackground" class="deleteBtn">Delete</button></td>
                 </tr>
             `);
         }
